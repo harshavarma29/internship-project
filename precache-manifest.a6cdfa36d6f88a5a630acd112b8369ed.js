702a3d7554d842f4227c26599f88755d1d283e86
harshavarma29 <harshavarma29@gmail.com>
2020-11-24 13:48:36 +0530

    Updates

---
+++ precache-manifest.a6cdfa36d6f88a5a630acd112b8369ed.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98b047632bf0f5ce7c0c54a8a6065a56",
+    "url": "/internship-project/index.html"
+  },
+  {
+    "revision": "38726b3353c14dccf7b5",
+    "url": "/internship-project/static/css/main.7bf3f54b.chunk.css"
+  },
+  {
+    "revision": "222119b03f5d426adebb",
+    "url": "/internship-project/static/js/2.365dc3e2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/internship-project/static/js/2.365dc3e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38726b3353c14dccf7b5",
+    "url": "/internship-project/static/js/main.d53bd7cc.chunk.js"
+  },
+  {
+    "revision": "58c9ed36d2aba43dd441",
+    "url": "/internship-project/static/js/runtime-main.a4671d91.js"
+  },
+  {
+    "revision": "c6e97801d0d4225122dbba706a576e2d",
+    "url": "/internship-project/static/media/business.c6e97801.svg"
+  },
+  {
+    "revision": "aaf90db29fe43d8bcf882a91cc5e6534",
+    "url": "/internship-project/static/media/call.aaf90db2.svg"
+  },
+  {
+    "revision": "8942fbcbfc79d3c5dfc62d1e84340af5",
+    "url": "/internship-project/static/media/check.8942fbcb.svg"
+  },
+  {
+    "revision": "6cfee57d2609274819dad2e7a89b3737",
+    "url": "/internship-project/static/media/hub.6cfee57d.svg"
+  },
+  {
+    "revision": "ffaeebfa327a14009f42925083fa8746",
+    "url": "/internship-project/static/media/icws.ffaeebfa.png"
+  },
+  {
+    "revision": "5ea51096a3153184cb236c8c84c1dade",
+    "url": "/internship-project/static/media/integration.5ea51096.svg"
+  },
+  {
+    "revision": "11c94b043e93b3032f827ad2b4771721",
+    "url": "/internship-project/static/media/legacy.11c94b04.svg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/internship-project/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "689cce52da8df98ebb46337bd2495b3f",
+    "url": "/internship-project/static/media/mobile.689cce52.svg"
+  },
+  {
+    "revision": "cc1595a753798e12c4037201dd56b1c5",
+    "url": "/internship-project/static/media/motto.cc1595a7.svg"
+  },
+  {
+    "revision": "9ef6ddd0ed0f279d2bc6f3cf6b0669cc",
+    "url": "/internship-project/static/media/nav-menu.9ef6ddd0.svg"
+  },
+  {
+    "revision": "50a50a2f63167e5699cf220e33eab67d",
+    "url": "/internship-project/static/media/no_rest.50a50a2f.svg"
+  },
+  {
+    "revision": "38b619773b450f99f2a2bb24d9ab6abc",
+    "url": "/internship-project/static/media/paint.38b61977.svg"
+  },
+  {
+    "revision": "be30a0e6b8c71f0b435ef844b016479b",
+    "url": "/internship-project/static/media/privacy.be30a0e6.jpeg"
+  },
+  {
+    "revision": "20f61bf52da97b6d37e7189fc152f6bf",
+    "url": "/internship-project/static/media/products.20f61bf5.svg"
+  },
+  {
+    "revision": "c68dc658dcca09ad8d29c8812e1dceab",
+    "url": "/internship-project/static/media/remotely.c68dc658.svg"
+  },
+  {
+    "revision": "88ae0f0c56efc970fc8b6d976dd886f1",
+    "url": "/internship-project/static/media/scaling.88ae0f0c.svg"
+  },
+  {
+    "revision": "4924b36af395ec75329c41f0591364bb",
+    "url": "/internship-project/static/media/seo.4924b36a.svg"
+  },
+  {
+    "revision": "8e3366ba35afda6c8a158a478ae9a906",
+    "url": "/internship-project/static/media/upArrow.8e3366ba.svg"
+  },
+  {
+    "revision": "134e9a80bee7a861dd0ffea675cfaeae",
+    "url": "/internship-project/static/media/web.134e9a80.svg"
+  },
+  {
+    "revision": "235a8d763d2d7ed1a1e28a138c2c9221",
+    "url": "/internship-project/static/media/web.235a8d76.svg"
+  }
+]);
